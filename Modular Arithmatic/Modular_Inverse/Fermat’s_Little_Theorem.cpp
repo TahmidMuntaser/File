03b7176afad56cbc72044b2b74d67233fc7921e1
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2025-04-30 14:55:15 +0600

    Fermat’s_Little_Theorem.cpp added

---
+++ Modular Arithmatic/Modular_Inverse/Fermat’s_Little_Theorem.cpp
@@ -0,0 +1,92 @@
+
+#include<bits/stdc++.h>
+
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long
+#define nl "\n"
+#define mod 1000000007
+#define Y cout<<"YES"<<"\n"
+#define N cout<<"NO"<<"\n"
+#define vll vector<ll>
+#define inp(v) for (ll i = 0; i < n; ++i){ll kk; cin>>kk; v.push_back(kk);}
+#define out(v, n) for (ll i = 0; i < n; ++i){cout<<v[i]<<" ";}
+
+using namespace std;
+
+void solve();
+
+int main(int argc, char const *argv[])
+{
+    io
+    ll testcases = 1, cc = 0;
+    // cin>>testcases;
+ 
+    while(cc<testcases){
+        //cout<<"#Case "<<cc+1<<" :";
+        solve(); ++cc;
+    }
+    return 0;
+}
+
+ll binexp(ll a, ll b, ll m){
+    ll res = 1;
+    while(b > 0){
+        if(b & 1) res = (res * a) % m;
+        a = (a * a) % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+
+void solve(){
+    ll a, m;
+    cin>>a>>m;
+
+    ll ans = binexp(a, m-2, m); // a^(m-2) mod m
+    cout<<(a*ans)%m<<nl; 
+    // ( a*(a^m-2))%mod
+
+
+    // if m is prime
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// A lie is a lie. Just because they write it down and call it history, 
+// doesn’t make it the truth. we live in a world where seeing is not believing 
+// where only a few know what really happened.we live in a world where everything 
+// you know is wrong.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "Sniping your enemy is like hunting, Fire at the wrong moment and your chance 
+// will be forever lost. Patience, if we reveal our position to Amsel's men, this 
+// fountain will be our grave."
